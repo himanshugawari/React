c7e61c7696c0e8befbe4ac9ab9e76fb74fe94071
unknown <himanshu25031991@gmail.com>
2020-10-15 21:26:36 +0530

    functional component

---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// ES6 arrow functions
+const Greet = () => {
+  return <h1>Hello Himanshu</h1>;
+};
+
+// function Greet() {
+//   return <h1>Hello Himanshu</h1>;
+// }
+
+export default Greet;
