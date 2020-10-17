b94e3ed94fed7a62079c5e8129f98dc91b154b36
unknown <himanshu25031991@gmail.com>
2020-10-17 17:02:42 +0530

    event handlers

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import './App.css';
 // import Welcome from './components/Welcome';
 // import Hello from './components/Hello';
 // import Message from './components/Message';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className='App'>
-      <Counter />
+      <FunctionClick />
+      <ClassClick />
+      {/* <Counter /> */}
       {/* <Message /> */}
       {/* <Greet name='Bruce' heroName='Batman'>
         <p>This is children props</p>
